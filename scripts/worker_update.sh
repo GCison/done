4ff40312ad9a980a3ba57c2e3dcc02d325db0a1c
GCison <cison@email.com>
2022-10-31 10:54:16 -0300

    altera index html com atualizacao de api

--- scripts/worker_update.sh
+++ scripts/worker_update.sh
@@ -4,22 +4,24 @@ function main(){
     change_html
     git_update
 }
+
 function get_projecao(){
-    readonly local _value=$(curl -X GET https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/BR | jq '.projecao.populacao')
+    local _value=$(curl -X GET https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/BR | jq '.projecao.populacao')
     echo $_value
 }
 
 function change_html(){
-    local _file=~/cison/html/done/index.html
-    local _value=$(get_projecao)
-    local _command_sed=$(sed -i 's/[0-9]\{3,\}/'"$_value"'/g' $_file)
+    readonly local _file=~/cison/html/done/index.html
+    readonly local _value=$(get_projecao)
+    readonly local _commad_sed=$(sed -i 's/[0-9]\{3,\}/'"$_value"'/g' $_file)
 }
 
 function git_update(){
-    cd ~/cison/html/done
+    cd ~/cison/html/done/
     git add .
-    git commit -m "atualiza projecao"
+    git commit -m "altera index html com atualizacao de api"
     git push
+    termux-toast -c green "Site atualizado: $(get_projecao)"
 }
 
 main
