d8db208b3443aa55442069f99b4293ba9df643f1
GCison <cison@email.com>
2022-10-31 08:51:28 -0300

    atualiza projecao

---
+++ scripts/exec_proc.py
@@ -0,0 +1,12 @@
+import time
+import os
+import sys
+
+def worker_update():
+    time_update = int(sys.argv[1]) 
+
+    while True:
+        os.system("~/cison/html/done/scripts/worker_update.sh")
+        time.sleep(time_update)
+
+worker_update()
